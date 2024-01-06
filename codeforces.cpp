ed0981d5bb0e33d6c362e70b26b861e8b197f11e
Sayan Bhattacharjee <bsayan8086@gmail.com>
2024-01-06 18:41:00 +0530

    Update template

--- codeforces.cpp
+++ codeforces.cpp
@@ -2,17 +2,20 @@
 
 #include <bits/stdc++.h>
 
+typedef long double                 ld;
+typedef          long long int     lli;
+typedef unsigned long long int    ulli;
+
 using namespace std;
 
 
 int main(void) {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr);
 
 
     // input parsing
     vector<int> nums(10);
-    for(int i = 0; i < 10; cin >> nums[i++]);
+    for(auto& x : nums) cin >> x;
 
 
     return 0;
